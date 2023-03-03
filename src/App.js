942bce5f5006179315405883478da2b6e5a9d917
obed300 <obamfo@st.ug.edu.gh>
2023-03-03 10:30:07 +0000

    Make changes

--- src/App.js
+++ src/App.js
@@ -22,14 +22,5 @@ function App() {
 
   );
 }
-// <BrowserRouter>
-//   <Routes>
-//     <Route path="/" element={<Layout />}>
-//       <Route index element={<div>Home page</div>} />
-//       <Route path="about" element={<div>About page</div>} />
-//       <Route path="*" element={<div>If page not found it goes here</div>} />
-//     </Route>
-//   </Routes>
-// </BrowserRouter>
 
 export default App;
